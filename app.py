806140313168236e1c1678714e97ccfedbf8879c
Ortega Quintero <CMORTE1@mapfre.net>
2025-03-31 16:18:44 +0200

    Fix secret decoding for Streamlit deployment

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # ---------- Google Sheets Setup ----------
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds_dict = json.loads(str(st.secrets["gcp_service_account"]))
+creds_dict = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["gcp_service_account"].get("universe_domain", "googleapis.com")
+}
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 gclient = gspread.authorize(creds)
 sheet = gclient.open("HLP_Responses").sheet1
